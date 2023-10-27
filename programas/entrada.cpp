9f1a68e0b68f5d985c13ce62c20b4415ec7a1773
JAGURA07 <xo_628hod@hotmail.com>
2023-10-26 21:21:28 -0600

    Hola1

---
+++ programas/entrada.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<endl<<"Hola! Bienvenido"<<endl; 
+	cout<<"Que se te ofrece el dia de hoy??\n";
+	int a,b,c=0,d=0,e=0;
+       	cin>>a;
+	b = a * 2;
+	c = 7999 / a;
+	d = 55 + a;
+	e = 44 - a;
+	cout<<endl<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e;
+}
